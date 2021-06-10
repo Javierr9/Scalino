c759fd8aef8a9999a0529a7ac8c85fbab60a3a85
Mutiara Sintesana Prasetyo <mutiara.sintesana@gmail.com>
2021-06-10 19:52:41 +0700

    Switchnya nunggu user default - nico.

--- MVC-Template/Themes/UI/Toggle/CustomToggle.swift
+++ MVC-Template/Themes/UI/Toggle/CustomToggle.swift
@@ -7,14 +7,21 @@
 
 import UIKit
 
-class CustomToggle: UISwitch {
+class NotationToggle: UISwitch {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+      isOn = true
+        tintColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        onTintColor = #colorLiteral(red: 0.6291375756, green: 0.6193236709, blue: 0.7431026101, alpha: 1)
+        addTarget(self, action: #selector(toggleSwitched(_:)), for: .valueChanged)
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+      
     }
-    */
-
+    @objc func toggleSwitched(_ sender:UISwitch){
 }
+    
+    
+}
+
+
+// SWITCHNYA MASIH NUNGGU USER DEFAULT!!!
