a5008609e7a81206ee35f19f15c84d0824ad65f0
Mutiara Sintesana Prasetyo <mutiara.sintesana@gmail.com>
2021-06-10 20:13:34 +0700

    // NANDO N IVAN JANLUP SET TYPE BUTTONNYA JADI CUSTOM & STATE CONFIGNYA HIGHLIGHTED @ XIB! di back button

--- MVC-Template/Themes/UI/Buttons/BackButton.swift
+++ MVC-Template/Themes/UI/Buttons/BackButton.swift
@@ -7,5 +7,16 @@
 
 import UIKit
 
-class BackButton: UIButton{}
+class backButton: UIButton {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+       setImage(UIImage(systemName: "chevron.left.circle.fill"), for: .normal)
+        tintColor = #colorLiteral(red: 0.6291375756, green: 0.6193236709, blue: 0.7431026101, alpha: 1)
+        addTarget(self, action: #selector(backButtonPressed(_:)), for: .touchUpInside)
+    }
+    @objc func backButtonPressed(_ sender:UIButton){
+}
 
+}
+
+// NANDO N IVAN JANLUP SET TYPE BUTTONNYA JADI CUSTOM & STATE CONFIGNYA HIGHLIGHTED @ XIB!
