c36091d2aa81df13725b7637e34970b3c1bdd84a
Mutiara Sintesana Prasetyo <mutiara.sintesana@gmail.com>
2021-06-10 20:13:34 +0700

    // NANDO N IVAN JANLUP SET TYPE BUTTONNYA JADI CUSTOM & STATE CONFIGNYA HIGHLIGHTED @ XIB! di back button

--- MVC-Template/Themes/UI/Buttons/PillButton.swift
+++ MVC-Template/Themes/UI/Buttons/PillButton.swift
@@ -4,17 +4,32 @@
 //
 //  Created by Mutiara Prasetyo on 09/06/21.
 //
-
 import UIKit
+
+let padding: CGFloat = 32.0
 
 class PillButton: UIButton {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setTitleColor(#colorLiteral(red: 0.6291375756, green: 0.6193236709, blue: 0.7431026101, alpha: 1), for: .normal)
+        titleLabel!.font = UIFont.systemFont(ofSize: 12.00, weight: .bold)
+        layer.borderWidth = 3.0
+        layer.borderColor = #colorLiteral(red: 0.6291375756, green: 0.6193236709, blue: 0.7431026101, alpha: 1)
+        layer.cornerRadius = 12.32
+        sizeToFit()
+        self.frame = CGRect(x: frame.minX, y: frame.minY, width: frame.width+padding, height: 25.0)
+        addTarget(self, action: #selector(pillButtonPressed(_:)), for: .touchDown)
     }
-    */
-
+    @objc func pillButtonPressed(_ sender:UIButton){
+        setTitleColor(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), for: .highlighted)
 }
+
+    override open var isHighlighted: Bool {
+        didSet {
+            backgroundColor = isHighlighted ? #colorLiteral(red: 0.6291375756, green: 0.6193236709, blue: 0.7431026101, alpha: 1) : #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+    }
+}
+
+
