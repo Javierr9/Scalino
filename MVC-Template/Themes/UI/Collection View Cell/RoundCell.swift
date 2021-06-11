8eaa494f7fb49b8ad137b785d651121a568c1584
Nate <natanielchris@icloud.com>
2021-06-11 11:08:27 +0700

    Kerjaan Nate Collection View Cell KELAR YA BEBSKY

--- MVC-Template/Themes/UI/Collection View Cell/RoundCell.swift
+++ MVC-Template/Themes/UI/Collection View Cell/RoundCell.swift
@@ -8,10 +8,18 @@
 import UIKit
 
 class RoundCell: UICollectionViewCell {
-
+    @IBOutlet weak var roundCellLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        contentView.layer.cornerRadius = 29.5
+        contentView.layer.borderWidth = 3
+        contentView.layer.borderColor = #colorLiteral(red: 0.6274509804, green: 0.6196078431, blue: 0.7411764706, alpha: 1)
+        let selectedView = UIView (frame: bounds)
+        selectedView.backgroundColor = #colorLiteral(red: 0.6274509804, green: 0.6196078431, blue: 0.7411764706, alpha: 1)
+        selectedView.layer.cornerRadius = 29.5
+        self.selectedBackgroundView = selectedView
+        
     }
 
 }
