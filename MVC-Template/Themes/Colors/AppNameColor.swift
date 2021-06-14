45cc390346d448a0ebd8ca778ed4825d74565827
Nate <natanielchris@icloud.com>
2021-06-14 09:18:46 +0700

    Colors kelar

--- MVC-Template/Themes/Colors/AppNameColor.swift
+++ MVC-Template/Themes/Colors/AppNameColor.swift
@@ -6,7 +6,12 @@
 //
 
 import Foundation
+import UIKit
 
-struct AppNameColor {
+let darkPurple = #colorLiteral(red: 0.2666666667, green: 0.2039215686, blue: 0.3098039216, alpha: 1)
+let lightPurple = #colorLiteral(red: 0.6196078431, green: 0.6196078431, blue: 0.7294117647, alpha: 1)
+let Purple = #colorLiteral(red: 0.337254902, green: 0.3019607843, blue: 0.5019607843, alpha: 1)
+let red = #colorLiteral(red: 0.9411764706, green: 0.4823529412, blue: 0.4980392157, alpha: 1)
+let green = #colorLiteral(red: 0.537254902, green: 0.8, blue: 0.6705882353, alpha: 1)
     
-}
+
