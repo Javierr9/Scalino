1d4b9a2f80e810d4e8199c268cc14753ae860020
FFFFSU <christiannico04@gmail.com>
2021-06-15 19:29:21 +0700

    added note long press gesture

---
+++ MVC-Template/Piano/View/PianoView/NoteLongPressGesture.swift
@@ -0,0 +1,13 @@
+//
+//  NoteLongPressGesture.swift
+//  MC2
+//
+//  Created by Nico Christian on 15/06/21.
+//
+
+import UIKit
+
+class NoteLongPressGesture: UILongPressGestureRecognizer {
+    var notePressed: Int?
+    var noteView: UIView?
+}
