830ecf383d0505e38847f796b5e3f4eee0910ed0
aiven <ivanvsigit@gmail.com>
2021-06-17 14:27:29 +0700

    Add Test My Knowledge Page

---
+++ MVC-Template/Piano/View/TestMyKnowledge/CollectionViewCells.swift
@@ -0,0 +1,19 @@
+//
+//  CollectionViewCells.swift
+//  MVC-Template
+//
+//  Created by Ivan Valentino Sigit on 17/06/21.
+//
+
+import UIKit
+
+class CollectionViewCells: UICollectionViewCell {
+        
+    @IBOutlet weak var xibLabel: UILabel!
+        
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
--- MVC-Template/Piano/View/TestMyKnowledge/TestMyKnowledge.swift
+++ MVC-Template/Piano/View/TestMyKnowledge/TestMyKnowledge.swift
@@ -9,12 +9,59 @@ import UIKit
 
 class TestMyKnowledge: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+    @IBOutlet weak var TitleTest: UILabel!
+    @IBOutlet weak var Question: UILabel!
+    @IBOutlet weak var QNumber: UILabel!
+    @IBOutlet weak var CollectionView: UICollectionView!
+    @IBOutlet weak var contentView: UIView!
+       
+       override init(frame: CGRect) {
+               super.init(frame: frame)
+               commonInit()
+           }
+           
+           required init?(coder aDecoder: NSCoder) {
+               super.init(coder: aDecoder)
+               commonInit()
+           }
+           
+           private func commonInit() {
+               let bundle = Bundle(for: type(of: self))
+               bundle.loadNibNamed("CustomView", owner: self, options: nil)
+               addSubview(contentView)
+               contentView.frame = bounds
+               contentView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+               contentView.backgroundColor = .red
+               initCollectionView()
+           }
 
-}
+           private func initCollectionView() {
+               let nib = UINib(nibName: "RoundCell", bundle: nil)
+               CollectionView.register(nib, forCellWithReuseIdentifier: "RoundCell")
+               CollectionView.dataSource = self
+               CollectionView.delegate = self
+           
+           
+       }
+   }
+
+   let CollectionViewCell:[String] = ["C", "G", "D", "A", "E", "B", "F", "F#", "C#", "B♭", "D#", "A♭"]
+
+   extension TestMyKnowledge: UICollectionViewDataSource, UICollectionViewDelegate {
+       func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+           //jumlah cell di collection view
+           return 12
+       }
+       
+       func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+           guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "RoundCell", for: indexPath) as? CollectionViewCells else {
+               fatalError("can't dequeue CustomCell")
+           }
+           cell.xibLabel.text = CollectionViewCell[indexPath.row]
+           return cell
+       }
+       
+       func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+           print("test")
+       }
+   }
