197d5cb4be32e345f572fcad8be32ca3b6a2a2bb
aiven <ivanvsigit@gmail.com>
2021-06-17 10:08:45 +0700

    Add buttons

--- MVC-Template/Piano/View/ChordPage/ChordPage.swift
+++ MVC-Template/Piano/View/ChordPage/ChordPage.swift
@@ -9,12 +9,32 @@ import UIKit
 
 class ChordPage: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+    @IBOutlet var Major1Btn : UIButton!
+    @IBOutlet var Minor1Btn : UIButton!
+    @IBOutlet var Minor2Btn : UIButton!
+    @IBOutlet var Major2Btn : UIButton!
+    @IBOutlet var Major3Btn : UIButton!
+    @IBOutlet var Minor3Btn : UIButton!
+    @IBOutlet var DimBtn : UIButton!
 
+    @IBOutlet weak var ChordPageView: UIView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        let bundle = Bundle(for: type(of: self))
+        bundle.loadNibNamed("CustomView", owner: self, options: nil)
+        addSubview(ChordPageView)
+        ChordPageView.frame = bounds
+        ChordPageView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+    }
 }
